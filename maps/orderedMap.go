0a066c4f9361ab82fc71ca6581d19d4f9931fe1c
rinki <rinki@list.ru>
2026-04-13 11:42:14 +0300

    Ordered maps remove broken type

--- maps/orderedMap.go
+++ maps/orderedMap.go
@@ -12,14 +12,6 @@ type OrderedMap[K comparable, V any] struct {
 	order []K
 }
 
-// OrderedFastDeleteMap represents a map whose elements are iterated over in the order they are inserted.
-// Unlike OrderedMap, requires more memory but deleting elements is fast.
-// To use this object, it is absolutely necessary to create the object using the NewOrderedFastDeleteMap constructor.
-type OrderedFastDeleteMap[K comparable, V any] struct {
-	*OrderedMap[K, V]
-	keyToOrderMap map[K]int
-}
-
 // NewOrderedMap allocates and initializes new object of type OrderedMap and returns a pointer to it.
 // It is absolutely necessary to create the OrderedMap object using this constructor.
 func NewOrderedMap[K comparable, V any](size int) *OrderedMap[K, V] {
@@ -31,19 +23,7 @@ func NewOrderedMap[K comparable, V any](size int) *OrderedMap[K, V] {
 	return &m
 }
 
-// NewOrderedFastDeleteMap allocates and initializes new object of type OrderedFastDeleteMap and returns a pointer to it.
-// It is absolutely necessary to create the OrderedFastDeleteMap object using this constructor.
-func NewOrderedFastDeleteMap[K comparable, V any](size int) *OrderedFastDeleteMap[K, V] {
-	m := OrderedFastDeleteMap[K, V]{
-		OrderedMap:    NewOrderedMap[K, V](size),
-		keyToOrderMap: make(map[K]int, size),
-	}
-
-	return &m
-}
-
 // Len returns the length of map.
-// Panics if original m type is OrderedFastDeleteMap and m is nil.
 func (m *OrderedMap[_, _]) Len() int {
 	if m == nil {
 		return 0
@@ -53,7 +33,6 @@ func (m *OrderedMap[_, _]) Len() int {
 }
 
 // Has checks if the map contains the given key.
-// Panics if original m type is OrderedFastDeleteMap and m is nil.
 func (m *OrderedMap[K, _]) Has(k K) bool {
 	if m == nil {
 		return false
@@ -64,7 +43,6 @@ func (m *OrderedMap[K, _]) Has(k K) bool {
 
 // GetValue returns a value by the given key.
 // If m is nil or k is not found, returns zero value of type V.
-// Panics if original m type is OrderedFastDeleteMap and m is nil.
 func (m *OrderedMap[K, V]) GetValue(k K) V {
 	if m == nil {
 		var v V
@@ -76,7 +54,6 @@ func (m *OrderedMap[K, V]) GetValue(k K) V {
 
 // GetAndCheck like GetValue and Has methods,
 // returns a value by the given key and checks if the map contains the key.
-// Panics if original m type is OrderedFastDeleteMap and m is nil.
 func (m *OrderedMap[K, V]) GetAndCheck(k K) (V, bool) {
 	if m == nil {
 		var v V
@@ -114,7 +91,6 @@ func (m *OrderedMap[K, _]) Delete(k K) {
 }
 
 // Iterate iterates over map elements in the order they are inserted.
-// Panics if original m type is OrderedFastDeleteMap and m is nil.
 func (m *OrderedMap[K, V]) Iterate() iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
 		if m == nil {
@@ -126,27 +102,4 @@ func (m *OrderedMap[K, V]) Iterate() iter.Seq2[K, V] {
 			}
 		}
 	}
-}
-
-// Set sets the given key-value pair into the map.
-// Panics if m is not initialized by NewOrderedFastDeleteMap constructor.
-func (m *OrderedFastDeleteMap[K, V]) Set(k K, v V) {
-	if !m.Has(k) {
-		m.keyToOrderMap[k] = len(m.order)
-		m.order = append(m.order, k)
-	}
-	m.data[k] = v
-}
-
-// Delete deletes an element by given key if the key exists.
-// Panics if m is not initialized by NewOrderedFastDeleteMap constructor.
-func (m *OrderedFastDeleteMap[K, _]) Delete(k K) {
-	if m == nil || !m.Has(k) {
-		return
-	}
-
-	delete(m.data, k)
-	i := m.keyToOrderMap[k]
-	m.order = append(m.order[:i], m.order[i+1:]...)
-	delete(m.keyToOrderMap, k)
 }
